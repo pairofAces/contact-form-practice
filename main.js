36ebf15319a2854d451eec6a1c08092235383094
pairofAces <karanchauhan16@gmail.com>
2020-12-12 19:47:49 -0500

    added email error logic

--- main.js
+++ main.js
@@ -4,5 +4,31 @@ $(document).ready(function () {
     $('.submit').click(function (e) {
         e.preventDefault();
         console.log('this button has just been clicked!!!')
+
+        //we need to get the individual input values and set to a variable
+        let email = $('.email').val()
+        let subject = $('.subject').val()
+        let message = $('.message').val()
+        let status = $('.status')
+        status.empty()
+
+        //create the error handling logic
+        if (email.length > 5 && email.includes('@') && email.includes('.')) {
+            status.append('<div>email is valid</div>')
+        } else {
+            status.append('<div>email is NOT valid</div>')
+        }
+
+        if (subject.length > 2) {
+            status.append('<div>Subject is valid</div>')
+        } else {
+            status.append('<div>Subject is NOT valid</div>')
+        }
+
+        if (message.lenght > 20) {
+            status.append('<div>Message is Valid</div>')
+        } else {
+            status.append('<div>Message is NOT Valid</div>')
+        }
     })
 })
