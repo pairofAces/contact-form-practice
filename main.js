c7c5eb8b3c944ac79b245c33aca800181e4a73b8
pairofAces <karanchauhan16@gmail.com>
2020-12-12 19:38:17 -0500

    creating click handler to listen for submit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log('this is working')
+
+$(document).ready(function () {
+    $('.submit').click(function (e) {
+        e.preventDefault();
+        console.log('this button has just been clicked!!!')
+    })
+})
